f71b9d0a521af3d86454fff99025371b434626ee
Husker <redfancoestar@gmail.com>
2019-09-14 16:33:42 +0300

    Add files via upload

---
+++ DarkMaterialSkin.java
@@ -0,0 +1,9 @@
+package com.husker.darkmaterial;
+
+import com.alee.managers.style.XmlSkin;
+
+public class DarkMaterialSkin extends XmlSkin {
+    public DarkMaterialSkin() {
+        super(DarkMaterialSkin.class,"resources/skin.xml");
+    }
+}
